39e66044addb9e98c88b90d39cc543446f105b1f
Anton Weiss <anton@otomato.link>
2018-06-20 13:30:19 +0300

    pass on all request headers for tracing and filtering

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,32 @@ function Poduct(name, price) {
   this.price = price; // default value
 }
 
+function getForwardedHeaders(req){
+    var headers = {};
+
+    var user_cookie = req.user_cookie;
+    if (user_cookie) {
+        headers['Cookie'] = 'user=' + user_cookie;
+    }
+    incoming_headers = [ 'x-request-id',
+                         'x-b3-traceid',
+                         'x-b3-spanid',
+                         'x-b3-parentspanid',
+                         'x-b3-sampled',
+                         'x-b3-flags',
+                         'x-ot-span-context'
+    ]
+
+    for (var ihdr of incoming_headers){
+        var val = req.headers[ihdr];
+        if (val){
+            headers[ihdr] = val;
+            console.log("incoming: "+ihdr+":"+val);
+        }
+    }
+    return headers;
+}
+
 exports.index = function(req, res){
   prod_url = "http://" + process.env.PRODUCT_SERVICE_HOST + ":" + process.env.PRODUCT_SERVICE_PORT + "/product"
   order_url = "http://" + process.env.ORDER_SERVICE_HOST + ":" + process.env.ORDER_SERVICE_PORT + "/orders"
@@ -18,12 +44,18 @@ exports.index = function(req, res){
   console.log(prod_url)
   var products = []
   var orders = []
-  request(prod_url, function (error, response, body) {
+  var fwdHeaders = getForwardedHeaders(req);
+  var options = {
+    url: prod_url,
+    headers: fwdHeaders
+  };
+  request(options, function (error, response, body) {
     if (!error && response.statusCode == 200)
     {
         console.log(body)
         products = JSON.parse(body)._items
-        request(order_url, function (error, response, body) {
+        request({ url: order_url,
+            headers: fwdHeaders}, function (error, response, body) {
         if (!error && response.statusCode == 200)
         {
             console.log(body)
@@ -39,8 +71,10 @@ exports.index = function(req, res){
 
 exports.register = function ( req, res ){
     console.log(req.body);
+    var fwdHeaders = getForwardedHeaders(req);
     prod_url = "http://" + process.env.PRODUCT_SERVICE_HOST + ":" + process.env.PRODUCT_SERVICE_PORT + "/product"
     request({ url: prod_url,
+              headers: fwdHeaders,
               method: 'POST',
               json: {"name": req.body.Product_name, "price": req.body.price, "stock": 0 }}, res.redirect('/'))
 
@@ -50,8 +84,13 @@ exports.order = function ( req, res ){
     console.log(req.body);
     var prod_url = "http://" + process.env.PRODUCT_SERVICE_HOST + ":" + process.env.PRODUCT_SERVICE_PORT + "/product"
     console.log(prod_url)
+    var fwdHeaders = getForwardedHeaders(req);
     var products = []
-    request(prod_url, function (error, response, body, products) {
+    var options = {
+        url: prod_url,
+        headers:fwdHeaders
+    };
+    request(options, function (error, response, body, products) {
         if (!error && response.statusCode == 200)
         {
             console.log(body);
@@ -69,6 +108,7 @@ exports.order = function ( req, res ){
                     order_url = "http://" + process.env.ORDER_SERVICE_HOST + ":" + process.env.ORDER_SERVICE_PORT + "/orders"
                     console.log(prod_id + " amount" + req.body.amount[0] + " totalSum " + total);
                     request({ url: order_url,
+                        headers:fwdHeaders,
                         method: 'POST',
                         json: {"productId": prod_id, "amount": req.body.amount[0], "totalSum": total }},
                                function(error, response, body){
